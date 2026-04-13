1a6977a93c2d6c877189a9b18171e7eaec6d3b3c
onlyoneprivate <pllm098@naver.com>
2026-04-13 23:15:07 +0900

    ✨Feat: 리뷰 작성 Controller•Service 구현

--- src/main/java/com/umc/umc_10th/domain/review/controller/ReviewController.java
+++ src/main/java/com/umc/umc_10th/domain/review/controller/ReviewController.java
@@ -1,4 +1,29 @@
 package com.umc.umc_10th.domain.review.controller;
 
+import com.umc.umc_10th.domain.review.dto.ReviewReqDTO;
+import com.umc.umc_10th.domain.review.dto.ReviewResDTO;
+import com.umc.umc_10th.domain.review.exception.code.ReviewSuccessCode;
+import com.umc.umc_10th.domain.review.service.ReviewService;
+import com.umc.umc_10th.global.apiPayLoad.ApiResponse;
+import com.umc.umc_10th.global.apiPayLoad.code.BaseSuccessCode;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/reviews")
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    @PostMapping("/{storeId}")
+    public ApiResponse<ReviewResDTO.CreateReview> createReview(
+            @PathVariable Long storeId,
+            @RequestBody @Valid ReviewReqDTO.CreateReview request) {
+
+        BaseSuccessCode code = ReviewSuccessCode.OK;
+        return ApiResponse.onSuccess(code, reviewService.createReview(request));
+
+    }
 }
