c97a44b351fdde6234ef8f361f0dd6e879350712
onlyoneprivate <pllm098@naver.com>
2026-04-13 22:32:26 +0900

    ✨Feat: 회원가입 요청/응답 DTO 생성

--- src/main/java/com/umc/umc_10th/domain/member/dto/MemberReqDTO.java
+++ src/main/java/com/umc/umc_10th/domain/member/dto/MemberReqDTO.java
@@ -1,4 +1,16 @@
 package com.umc.umc_10th.domain.member.dto;
 
+import com.umc.umc_10th.domain.member.enums.Gender;
+
+import java.time.LocalDate;
+
 public class MemberReqDTO {
+
+    // 회원 가입
+    public record SignUp(
+            String nickname,
+            Gender gender,                  // MALE or FEMALE
+            LocalDate birth,
+            String password
+    ) {}
 }
--- src/main/java/com/umc/umc_10th/domain/member/dto/MemberResDTO.java
+++ src/main/java/com/umc/umc_10th/domain/member/dto/MemberResDTO.java
@@ -1,4 +1,16 @@
 package com.umc.umc_10th.domain.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.time.LocalDateTime;
+
 public class MemberResDTO {
+
+    // 회원가입
+    public record SignUp(
+            Long memberId,
+            String nickname,
+            @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+            LocalDateTime createdAt
+    ){}
 }
