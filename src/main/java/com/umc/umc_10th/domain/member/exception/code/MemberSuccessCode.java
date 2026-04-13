9b90741976df3b0ab11d6885048b1281baa94e6d
onlyoneprivate <pllm098@naver.com>
2026-04-13 22:31:29 +0900

    ✨Feat: 회원가입 성공 코드 추가

--- src/main/java/com/umc/umc_10th/domain/member/exception/code/MemberSuccessCode.java
+++ src/main/java/com/umc/umc_10th/domain/member/exception/code/MemberSuccessCode.java
@@ -1,4 +1,19 @@
 package com.umc.umc_10th.domain.member.exception.code;
 
-public enum MemberSuccessCode {
+import com.umc.umc_10th.global.apiPayLoad.code.BaseSuccessCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum MemberSuccessCode implements BaseSuccessCode {
+
+    OK(HttpStatus.OK, "MEMBER200_01", "회원가입이 성공적으로 완료되었습니다."),
+
+    ;
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
 }
