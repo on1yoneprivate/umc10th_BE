ac42b4913c77ff3ea0c3010a474768d8002b9a20
onlyoneprivate <pllm098@naver.com>
2026-04-13 23:14:41 +0900

    ✨Feat: 리뷰 작성 요청/응답 DTO 작성

--- src/main/java/com/umc/umc_10th/domain/review/dto/ReviewReqDTO.java
+++ src/main/java/com/umc/umc_10th/domain/review/dto/ReviewReqDTO.java
@@ -1,4 +1,23 @@
 package com.umc.umc_10th.domain.review.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+
+@Getter
 public class ReviewReqDTO {
+
+    // 리뷰 작성
+    public record CreateReview(
+        @NotNull(message = "평점은 필수 입력입니다.")
+        @Min(value = 0, message = "평점은 0점 이상이어야 합니다.")
+        @Max(value = 5, message = "평점은 5점 이하여야 합니다.")
+        Double rating,
+
+        @NotBlank(message = "리뷰 내용을 작성해 주세요.")
+        String content
+    ) {}
+
 }
