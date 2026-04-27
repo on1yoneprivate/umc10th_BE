7c3be799fc459c98531633359eb2f843a74fb04d
onlyoneprivate <pllm098@naver.com>
2026-04-27 23:16:17 +0900

    ✨Feat: 마이페이지 조회 기능 구현

--- src/main/java/com/umc/umc_10th/domain/member/service/MemberService.java
+++ src/main/java/com/umc/umc_10th/domain/member/service/MemberService.java
@@ -2,7 +2,10 @@ package com.umc.umc_10th.domain.member.service;
 
 import com.umc.umc_10th.domain.member.dto.MemberReqDTO;
 import com.umc.umc_10th.domain.member.dto.MemberResDTO;
+import com.umc.umc_10th.domain.member.entity.Member;
+import com.umc.umc_10th.domain.member.exception.code.MemberErrorCode;
 import com.umc.umc_10th.domain.member.repository.MemberRepository;
+import com.umc.umc_10th.global.apiPayLoad.exception.ProjectException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,7 +15,23 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
 
-    public final MemberResDTO.SignUp signUp(MemberReqDTO.SignUp request) {
+    public MemberResDTO.SignUp signUp(MemberReqDTO.SignUp request) {
         return null;
+    }
+
+    public MemberResDTO.MyPage getMyPage(Long memberId) {
+
+        Member member = memberRepository.findById(memberId)
+                .orElseThrow(() -> new ProjectException(MemberErrorCode.MEMBER_NOT_FOUND));
+
+        return new MemberResDTO.MyPage(
+                member.getId(),
+                member.getNickname(),
+                member.getEmail(),
+                member.getPhone(),
+                member.getPhone() != null,
+                member.getPoint(),
+                null
+        );
     }
 }
